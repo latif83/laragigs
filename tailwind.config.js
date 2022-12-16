a0f86b0c5412bb0e7a6539afec432c36a1780ceb
latif83 <latifm8360@gmail.com>
2022-12-16 10:38:00 +0000

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        laravel: "#ef3b2d",
+        see: "green"
+    },
+    },
+  },
+  plugins: [],
+}
